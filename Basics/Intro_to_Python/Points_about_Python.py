e2d6e23b9e5654e9146a559742d611ab6e0cca97
Titan-11 <160115504+Aarushcpp@users.noreply.github.com>
2024-05-01 09:37:29 +0530

    Update Points_about_Python.py

--- Basics/Intro_to_Python/Points_about_Python.py
+++ Basics/Intro_to_Python/Points_about_Python.py
@@ -1,1 +1,32 @@
+Python3
+-----------------------Terminal------------------------------
+
+Go to your terminal,give command python3 to start a repl of python3
+
+-----------------------------
+
+Programming-
+  Programming is a process for us to tell computers what to do.Computer is a very dumb language and we tell the computer what to do.Hence we program computers.
+  
+Python-
+  It is a dynamically typed,general purpose programming language that supports an object oriented programming approach as well as functional programming approach.
+  It is interpreted and a high level programming language.
+
+Features of Python-
+  It is simple and easy to understand.
+  It is interpreted and platform independent which makes debugging very easy.
+  Python is an open source programming language.
+  Python provides very big library support.Some of the popular libraries are Tensorflow,OpenCV,Scikit-Learn,Selenium.
+  It is possible to integrate other programming languages with Python.
+
+Use Cases of Python-
+    1.It is used for Data Visualization to create plots and graphical representations.
+    2.Python helps in Data Analytics to analyze nd understand raw data for insights and trends.
+    3.It is used in AI and Machine Learning to simulate human behaviour and learn from past data without hard coding.
+    4.It is used to create web applications.
+    5.It can be used to handle databases.
+    6.It is used in buisness and accounting to perform complex mathematics operations along with qualitative and quantitative analysis.
+
+
+
 
