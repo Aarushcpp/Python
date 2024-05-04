046063d29817c732cfd2133e18f92ca13ddfc139
Titan-11 <160115504+Aarushcpp@users.noreply.github.com>
2024-05-04 15:54:44 +0530

    Create Typecasting.py

---
+++ Basics/Intro_to_Python/Typecasting.py
@@ -0,0 +1,6 @@
+#Typecasting is the process of conversion of one type of data to another type of data.
+#Two types of typecasting 1.Implicit 2.Explicit
+#1.Explicit-This is done by the programmer or by manual intervention.We have built-in functions for conversion of this.
+#2.Implicit-This is done by python interpreter.
+#int(),float(),str(),tuple(),list(),dict() etc..
+#to prevent data loss implicit conversion does typecasting of lower data types to higher data types
