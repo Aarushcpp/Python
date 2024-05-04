103c476653464869cfc3063ea0b9b18b01d2cc73
Titan-11 <160115504+Aarushcpp@users.noreply.github.com>
2024-05-04 16:17:30 +0530

    Create Strings.py

---
+++ Basics/Intro_to_Python/Strings.py
@@ -0,0 +1,13 @@
+#In python,we have a sequence of characters or a collection of characters which is known as string.
+#A string has indexing from 0 to n-1.where n is the number of characters in a string.
+#A multi-line string can be made by using triple single quotes or triple double quotes.
+temp = '''A house was there which was very old,
+it was very small and scary,
+one day a man came to stay in the house,
+after that day no one came'''
+print(temp)
+print(temp[0],temp[1],temp[2],temp[3])
+#Check working
+a = "Aarush"
+print(a[3])
+print(a[2],sep=" ~ ",end="\n")
